a7461c2cbe26a7eba1587467f111b0fe35236fd3
jfqdev <jfqdev@gmail.com>
2021-05-22 22:24:46 -0300

    Add credential to index

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 const { Sequelize } = require('sequelize');
-const dbCredentials = require('./dbCredenciales');
+const dbCredentials = require('./dbCredentials');
 
 const sequelize = new Sequelize(dbCredentials.databaseName, dbCredentials.user, dbCredentials.password, {
     host: dbCredentials.host,
