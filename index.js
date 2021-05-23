9ef95b056303d0f22ccaa371f28f064250d494d8
Lucas Pérez <lperez@infoil.com.ar>
2021-05-22 21:31:03 -0300

    conexion a bd local

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
-let hola = 'Hola Joaco'
+var express = require('express');
+var app = express();
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('database', 'user', 'pass.', {
+    host: 'localhost',
+    dialect: 'mysql'
+  });
+
+    sequelize.authenticate().then(() => {
+        console.log('Atroden');
+    }).catch(() => {
+        console.log('Chupala');
+    })
 
 
-console.log(hola)
+app.get('/', function (req, res) {
+	  res.send('Hello World!');
+});
+app.listen(3000, function () {
+	  console.log('Aplicacion de pruebas escuchando en el puerto 3000!');
+});
